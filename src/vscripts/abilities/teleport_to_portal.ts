e16335995eeecf1bd06d15dcbf8ee79d0f6484bf
Pascal Blessing <pascal.blessing@gmx.at>
2021-09-17 14:36:19 +0200

    Prevent portal collision

--- src/vscripts/abilities/teleport_to_portal.ts
+++ src/vscripts/abilities/teleport_to_portal.ts
@@ -1,5 +1,6 @@
 import { HeroCharacters, HeroString, HeroStringToHeroEnum } from "../constants";
 import { BaseAbility, registerAbility } from "../lib/dota_ts_adapter";
+import { equal, isBetweenInclusive } from "../utils";
 
 @registerAbility()
 export class teleport_to_portal extends BaseAbility
@@ -26,7 +27,38 @@ export class teleport_to_portal extends BaseAbility
 
         const figure = Entities.FindByClassname(undefined, HeroCharacters[HeroStringToHeroEnum[figureName]]);
 
+        const targetPosition = target.GetAbsOrigin();
+
         if(!figure) return;
+
+        if(GameRules.Addon.CharacterEntities.some(x =>
+        {
+            return equal(x.GetAbsOrigin(), targetPosition);
+        }))
+        {
+            return;
+        }
+        if(GameRules.Addon.CurrentMovements.some(x =>
+        {
+            if(Math.abs(x.start.x - x.end.x) < 128)
+            {
+                const isCrossingVertical = Math.abs(x.start.x - targetPosition.x) < 128 && isBetweenInclusive(targetPosition.y, x.unit.GetAbsOrigin().y, x.end.y);
+                return isCrossingVertical;
+            }
+            else if(Math.abs(x.start.y - x.end.y) < 128)
+            {
+                const isCrossingHorizontal = Math.abs(x.start.y - targetPosition.y) < 128 && isBetweenInclusive(targetPosition.x, x.unit.GetAbsOrigin().x, x.end.x);
+                return isCrossingHorizontal;
+            }
+            else
+            {
+                const isCrossingStair = equal(x.end, targetPosition);
+                return isCrossingStair;
+            }
+        }))
+        {
+            return;
+        }
 
         figure.SetAbsOrigin(target.GetAbsOrigin() + Vector(0, 0, 92) as Vector);
         GameRules.Addon.IsTimerRunning = true;
