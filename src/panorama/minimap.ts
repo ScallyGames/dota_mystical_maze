e65ca2ae5614d61a5444d7272d5f596702f55d07
Pascal Blessing <pblessing.mmt-b2017@fh-salzburg.ac.at>
2021-09-19 21:16:48 +0200

    Add minimap icons

--- src/panorama/minimap.ts
+++ src/panorama/minimap.ts
@@ -5,6 +5,29 @@ const fullMapSize = 8192 * 2;
 
 const allPanels : Panel[] = [];
 
+const iconEntities = [
+    "barbarian",
+    "warrior",
+    "alchemist",
+    "archer",
+    "barbarian_weapon_area",
+    "warrior_weapon_area",
+    "alchemist_weapon_area",
+    "archer_weapon_area",
+];
+
+const iconReferences = new Map([
+    ["barbarian", { panel: $('#hero_barbarian'), lastPosition: { x: 0, y: 0 }, iconSize: 32 }],
+    ["warrior", { panel: $('#hero_warrior'), lastPosition: { x: 0, y: 0 }, iconSize: 32 }],
+    ["alchemist", { panel: $('#hero_alchemist'), lastPosition: { x: 0, y: 0 }, iconSize: 32 }],
+    ["archer", { panel: $('#hero_archer'), lastPosition: { x: 0, y: 0 }, iconSize: 32 }],
+    ["7_18_barbarian_weapon_anchor", { panel: $('#weapon_barbarian'), lastPosition: { x: 0, y: 0 }, iconSize: 32 }],
+    ["7_17_warrior_weapon_anchor", { panel: $('#weapon_warrior'), lastPosition: { x: 0, y: 0 }, iconSize: 32 }],
+    ["6_22_alchemist_weapon_anchor", { panel: $('#weapon_alchemist'), lastPosition: { x: 0, y: 0 }, iconSize: 32 }],
+    ["8_21_archer_weapon_anchor", { panel: $('#weapon_archer'), lastPosition: { x: 0, y: 0 }, iconSize: 32 }],
+    ["6_21_portal_exit_anchor", { panel: $('#exit'), lastPosition: { x: 0, y: 0 }, iconSize: 32 }],
+]);
+
 
 const minimapSize = 244 + 128 * 1.5;
 const padding = 8;
@@ -26,7 +49,8 @@ hudSkinMinimap.style.transform = 'scaleX(-1)';
 
 
 
-let mapBounds = { left: 0, top: 0, bottom: 0, right: 0 }
+let mapBounds = { left: -1024, top: 1024, bottom: -1024, right: 1024 }
+let boundsChanged = true;
 
 
 const minimapTilesOverlayPadding = $('#minimap-tiles-overlay-padding');
@@ -40,6 +64,7 @@ function OnMinimapChanged(table_name : any, key : any, data : any) {
 
 
     mapBounds = data.mapBounds;
+    boundsChanged = true;
 
     if(mapBounds)
     {
@@ -83,6 +108,48 @@ CustomNetTables.SubscribeNetTableListener("minimap_info" as any as never, OnMini
 
 function OnTick()
 {
+    iconReferences.forEach(( _, iconName ) =>
+    {
+        const heroEntityIds = Entities.GetAllEntitiesByName(iconName);
+
+        if(heroEntityIds.length === 0) return;
+
+        const heroPosition = Entities.GetAbsOrigin(heroEntityIds[0]);
+
+        let iconReference = iconReferences.get(iconName)!;
+
+        if( !boundsChanged &&
+            heroPosition[0] == iconReference.lastPosition.x &&
+            heroPosition[1] == iconReference.lastPosition.y
+        ) return;
+
+        iconReference.lastPosition =
+        {
+            x: heroPosition[0],
+            y: heroPosition[1],
+        };
+
+        const heroPositionMinimapSpace = {
+            x: (heroPosition[0] - mapBounds.left) / (mapBounds.right - mapBounds.left) * minimapSize,
+            y: (heroPosition[1] - mapBounds.bottom) / (mapBounds.top - mapBounds.bottom) * minimapSize,
+        }
+
+        let newMargin =  `
+            ${minimapSize - heroPositionMinimapSpace.y - iconReference.iconSize / 2}px
+            ${minimapSize - heroPositionMinimapSpace.x - iconReference.iconSize / 2}px
+            ${heroPositionMinimapSpace.y - iconReference.iconSize / 2}px
+            ${heroPositionMinimapSpace.x - iconReference.iconSize / 2}px
+        `;
+        newMargin = newMargin.replace(/[\r\n\s]+/g, ' ');
+        iconReference.panel.style.margin = newMargin;
+        iconReference.panel.style.visibility = "visible";
+
+
+        iconReferences.set(iconName, iconReference)!;
+    });
+
+
+
     if(GameUI.IsMouseDown(0))
     {
         let cursorPosition = GameUI.GetCursorPosition();
@@ -111,6 +178,7 @@ function OnTick()
         }
 
     }
+    boundsChanged = false;
 }
 
 function isWithin(x: number, min: number, max: number): boolean
